nxp-archive/phealth_gnuarm

xtensa: Fix PR target/78603
2016-11-29  Max Filippov  <jcmvbkbc@gmail.com>
gcc/
	* config/xtensa/xtensa.c (hwloop_optimize): Don't emit zero
	overhead loop start between a call and its CALL_ARG_LOCATION
	note.



git-svn-id: svn://gcc.gnu.org/svn/gcc/trunk@242979 138bc75d-0d04-0410-961f-82ee72b054a4

gcc/config/xtensa/xtensa.c
@@ -4167,7 +4167,10 @@ hwloop_optimize (hwloop_info loop)
       entry_after = BB_END (entry_bb);
       while (DEBUG_INSN_P (entry_after)
              || (NOTE_P (entry_after)
-                 && NOTE_KIND (entry_after) != NOTE_INSN_BASIC_BLOCK))
+                 && NOTE_KIND (entry_after) != NOTE_INSN_BASIC_BLOCK
+		 /* Make sure we don't split a call and its corresponding
+		    CALL_ARG_LOCATION note.  */
+                 && NOTE_KIND (entry_after) != NOTE_INSN_CALL_ARG_LOCATION))
         entry_after = PREV_INSN (entry_after);
 
       emit_insn_after (seq, entry_after);

mirko/gcc

git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@242979 138bc75d-0d04-0410-961f-82ee72b054a4
Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

gcc/config/xtensa/xtensa.c
@@ -4286,7 +4286,10 @@ hwloop_optimize (hwloop_info loop)
     {
       while (DEBUG_INSN_P (entry_after)
              || (NOTE_P (entry_after)
-		 && NOTE_KIND (entry_after) != NOTE_INSN_BASIC_BLOCK))
+                 && NOTE_KIND (entry_after) != NOTE_INSN_BASIC_BLOCK
+         /* Make sure we don't split a call and its corresponding
+            CALL_ARG_LOCATION note.  */
+                 && NOTE_KIND (entry_after) != REG_CALL_ARG_LOCATION))
         entry_after = PREV_INSN (entry_after);
 
       emit_insn_after (seq, entry_after);